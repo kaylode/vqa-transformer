b369df19da0bc75ef029fe5c75a05fc8a3314078
kaylode <kayp.kieran@gmail.com>
2021-09-06 19:36:22 +0700

    clean datasets

--- datasets/cocoset.py
+++ datasets/cocoset.py
@@ -13,7 +13,7 @@ from augmentations.transforms import get_resize_augmentation, get_augmentation, 
 from pycocotools.coco import COCO
 from torch.utils.data import Dataset, DataLoader
 
-from .utils import create_masks, make_feature_batch, decode_tsv
+from .utils import create_masks, make_feature_batch
 from utils.utils import draw_image_caption
 
 class CocoDataset(Dataset):
@@ -209,123 +209,6 @@ class CocoDataset(Dataset):
 
     def __str__(self): 
         s1 = "Number of images: " + str(len(self.image_ids)) + '\n'
-        s2 = "Number of texts: " + str(len(self.coco.getAnnIds())) + '\n'
-        return s1 + s2
-
-
-class BottomUpDataset(Dataset):
-    def __init__(self, root_dir, tsv_path, ann_path, tokenizer):
-        super().__init__()
-
-        self.use_attr = False
-        self.root_dir = root_dir
-        self.ann_path = ann_path
-        self.tokenizer = tokenizer
-        self.coco = COCO(ann_path)
-        self.fns = decode_tsv(tsv_path)
-
-    def __len__(self):
-        return len(self.fns)
-
-    def load_image(self, image_id):
-        image_info = self.coco.loadImgs(image_id)[0]
-        image_path = os.path.join(self.root_dir, image_info['file_name'])
-        return image_path
-
-    def load_annotations(self, image_id, return_all=False):
-        # get ground truth annotations
-        annotations_ids = self.coco.getAnnIds(imgIds=image_id)
-
-        if not return_all:
-            if len(annotations_ids)>0:
-                ann_id = random.choice(annotations_ids)
-            else:
-                assert f"Image id: {image_id} is missing"
-            anns = self.coco.loadAnns(ann_id)[0]['caption']
-        else:
-            anns = self.coco.loadAnns(annotations_ids)
-            anns = [i['caption'] for i in anns]
-        return anns
-
-    def __getitem__(self, index):
-        item = self.fns[index]
-        image_id = item['img_id']
-        obj_id = item["objects_id"] 
-        obj_conf = item["objects_conf"]
-        num_boxes = item['num_boxes']
-
-        if self.use_attr:
-            attrs_id = item["attrs_id"] 
-            attrs_conf = item["attrs_conf"] 
-
-        boxes = item["boxes"] 
-        np_feats = item["features"].reshape(num_boxes, -1).setflags(write=1)
-        feats = torch.from_numpy(np_feats)
-
-        location_feats = np.concatenate([boxes.reshape(num_boxes, -1), obj_id.reshape(num_boxes, -1)], axis=1)
-        location_feats = torch.from_numpy(location_feats)
-        image_path = self.load_image(image_id)
-        text = self.load_annotations(image_id)
-        
-        return {
-            'image_id': image_id,
-            'image_path': image_path,
-            'text': text,
-            "feats": feats.float(),
-            "loc_feats": location_feats.float(),
-        }
-
-    def collate_fn(self, batch):
-        
-        image_ids = [s['image_id'] for s in batch]
-        image_paths = [s['image_path'] for s in batch]
-        feats = torch.stack([s['feats'] for s in batch])
-        loc_feats = torch.stack([s['loc_feats'] for s in batch])
-        image_masks = torch.ones(feats.shape[:2])
-
-        image_names = []
-        ori_imgs = []
-        for image_path in image_paths:
-            image_names.append(os.path.basename(image_path))
-
-        for image_path in image_paths:
-            ori_img = cv2.imread(image_path)
-            ori_img = cv2.cvtColor(ori_img, cv2.COLOR_BGR2RGB)
-            ori_imgs.append(ori_img)
-
-        texts = [s['text'] for s in batch]
-        
-        tokens = self.tokenizer(texts, truncation=True)
-        tokens = [np.array(i) for i in tokens['input_ids']]
-
-        texts_ = make_feature_batch(
-            tokens, pad_token=self.tokenizer.pad_token_id)
-        
-        texts_inp = texts_[:, :-1]
-        texts_res = texts_[:, 1:]
-
-        text_masks = create_masks(
-            texts_inp,
-            pad_token=self.tokenizer.pad_token_id, 
-            is_tgt_masking=True)
-        
-        texts_inp = texts_inp.squeeze(-1)
-        
-        return {
-            'image_ids': image_ids,
-            'image_names': image_names,
-            'ori_imgs': ori_imgs,
-            'feats': feats,
-            'loc_feats': loc_feats,
-            'image_masks': image_masks.long(),
-            'tgt_texts_raw': texts,
-            'texts_inp': texts_inp.long(),
-            'texts_res': texts_res.long(),
-            'text_masks': text_masks.long(),
-        }
-
-    def __str__(self): 
-        s1 = "Number of images: " + str(len(self.fns)) + '\n'
         s2 = "Number of texts: " + str(len(self.coco.getAnnIds())) + '\n'
         return s1 + s2
 
--- datasets/dataloader.py
+++ datasets/dataloader.py
@@ -1,4 +1,4 @@
-from .cocoset import CocoDataset, BottomUpDataset, NumpyFeatureDataset
+from .cocoset import CocoDataset, NumpyFeatureDataset
 from torch.utils.data import DataLoader
 from torchtext.legacy.data import BucketIterator
 
@@ -60,60 +60,6 @@ class RawTextLoader(DataLoader):
         self.collate_fn = self.dataset.collate_fn
         
         super(RawTextLoader, self).__init__(
-            self.dataset,
-            batch_size=batch_size,
-            num_workers=2,
-            pin_memory=True,
-            collate_fn=self.collate_fn)
-
-class BottomUpLoader(BucketIterator):
-    """
-    Use BucketIterator to make texts of same length into batch
-    """
-    def __init__(self, 
-                batch_size,
-                root_dir,
-                tsv_path, 
-                ann_path, 
-                tokenizer,
-                device,
-                **kwargs):
-       
-        self.dataset = BottomUpDataset(
-            root_dir, tsv_path, ann_path, tokenizer)
-
-        self.tokenizer = self.dataset.tokenizer
-        self.collate_fn = self.dataset.collate_fn
-        
-        super(BottomUpLoader, self).__init__(
-            self.dataset,
-            batch_size=batch_size,
-            device=device, 
-            sort_key=lambda x: len(x['text']),
-            repeat=True, # Repeat the iterator for multiple epochs.
-            sort=False,  # Sort all examples in data using `sort_key`.
-            shuffle=True, # Shuffle data on each epoch run.
-            sort_within_batch=True) # Use `sort_key` to sort examples in each batch.
-
-class RawBottomUpLoader(DataLoader):
-    """
-    Use DataLoader to make texts into batch
-    """
-    def __init__(self, 
-                batch_size,
-                root_dir,
-                tsv_path, 
-                ann_path, 
-                tokenizer,
-                **kwargs):
-       
-        self.dataset = BottomUpDataset(
-            root_dir, tsv_path, ann_path, tokenizer)
-
-        self.tokenizer = self.dataset.tokenizer
-        self.collate_fn = self.dataset.collate_fn
-        
-        super(RawBottomUpLoader, self).__init__(
             self.dataset,
             batch_size=batch_size,
             num_workers=2,
